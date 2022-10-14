de1c87b89660e1822ccb96536ceab382a6eeb54f
duynvse151394 <duynvse151394@fpt.edu.vn>
2022-10-14 10:44:22 +0700

    update

--- src/com/nghiapd/mathutil/main/MathUtil.java
+++ src/com/nghiapd/mathutil/main/MathUtil.java
@@ -18,9 +18,9 @@ public class MathUtil {
     //chi tinh n tu` 1 => 20
     public static long getFactorial(int n) {
         //if n <0 va n > 20
-        if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("Invalid n. N must be between 0 -> 20");
-        }
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid n. N must be between 0 -> 20");
+//        }
 //        if (n == 0 || n == 1) {
 //            return 1;
 //        }
