fa2f245072b26b46745dafceb4acb7d3eb0b165e
duynvse151394 <duynvse151394@fpt.edu.vn>
2022-10-14 10:46:24 +0700

    update getf() to see green again

--- src/com/nghiapd/mathutil/main/MathUtil.java
+++ src/com/nghiapd/mathutil/main/MathUtil.java
@@ -18,12 +18,12 @@ public class MathUtil {
     //chi tinh n tu` 1 => 20
     public static long getFactorial(int n) {
         //if n <0 va n > 20
-//        if (n < 0 || n > 20) {
-//            throw new IllegalArgumentException("Invalid n. N must be between 0 -> 20");
-//        }
-//        if (n == 0 || n == 1) {
-//            return 1;
-//        }
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid n. N must be between 0 -> 20");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
 //        long product = 1;
 //        for (int i = 2; i <= n; i++) {
 //            product *= i;
